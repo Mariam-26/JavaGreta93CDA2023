662b23b155c15134936fed58236eb630a9ee5ad9
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-07-05 22:59:31 +0200

    modification

---
+++ montreuil/src/heritage/Salarie.java
@@ -0,0 +1,20 @@
+package heritage;
+
+public class Salarie {
+	private double salaireBase;
+
+	public double getSalaireBase() {
+		return salaireBase;
+	}
+
+	public double getSalaire() {
+		return salaireBase;
+	}
+
+	public Salarie(double salaireBase) {
+		super();
+		this.salaireBase = salaireBase;
+	}
+	
+
+}
